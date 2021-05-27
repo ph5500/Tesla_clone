f11f508794d98d627426fc75d70051239b372203
ph5500 <philfives@gmail.com>
2021-05-27 13:19:39 -0600

    added Items of images and text content for different products

--- src/App.js
+++ src/App.js
@@ -1,11 +1,76 @@
 import logo from "./logo.svg";
 import "./App.css";
-import TeslaLogo from "./assets/TeslaLogo.png";
 import Header from "./components/Header";
+import ModelS from "./assets/ModelS.jpeg";
+import SolarPanels from "./assets/SolarPanels.jpeg";
+import ModelX from "./assets/ModelX.jpeg";
+import SolarRoof from "./assets/SolarRoof.jpeg";
+import TeslaLogo from "./assets/TeslaLogo.png";
+import TslaCharger from "./assets/TslaCharger.jpeg";
+import ModelY from "./assets/ModelY.jpeg";
+import Model3 from "./assets/Model3.jpeg";
+import Item from "./components/Item";
+
 function App() {
   return (
-    <div>
+    <div className="App">
       <Header />
+      <div className="app_items_container">
+        <Item
+          title="Lowest Cost Solar Panels in the country"
+          desc="money-back guarantee"
+          descLink=""
+          backgroundImg={SolarPanels}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          twoButtons="true"
+          first
+        />
+        <Item
+          title="ModelS"
+          desc="$69,420"
+          descLink=""
+          backgroundImg={ModelS}
+          leftBtnTxt="CUSTOM ORDER"
+          leftBtnLink=""
+          rightBtnTx="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Model 3"
+          desc="Money-back guarantee"
+          descLink=""
+          backgroundImg={Model3}
+          leftBtnTxt="CUSTOM ORDER"
+          leftBtnLink=""
+          rightBtnTx="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Model X"
+          desc="Money-back guarantee"
+          descLink=""
+          backgroundImg={ModelX}
+          leftBtnTxt="CUSTOM ORDER"
+          leftBtnLink=""
+          rightBtnTx="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="ModelY"
+          desc="Money-back guarantee"
+          descLink=""
+          backgroundImg={ModelY}
+          leftBtnTxt="CUSTOM ORDER"
+          leftBtnLink=""
+          rightBtnTx="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+      </div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import TeslaLogo from "../assets/TeslaLogo.png";
 import "./Header.css";
+import TeslaLogoSmall from "../assets/TeslaLogoSmall.svg";
 
 const Header = () => {
   return (
     <div className="header">
       <div className="header_logo">
-        <img src={TeslaLogo} alt="Tesla_logo"></img>
+        <img src={TeslaLogoSmall} alt="Tesla_logo"></img>
       </div>
       <div className="header_center">
         <p>Model S</p>
